67464208844d01860e33d6ee791dc96832b6895f
DJTheCreator <diegocajiao@outlook.com>
2024-11-02 22:24:17 -0400

    Final Graphs

--- DataAnalysis/generateGraphs.py
+++ DataAnalysis/generateGraphs.py
@@ -7,6 +7,9 @@ import os
 import openpyxl
 from numpy import median, std, mean
 
+ax = plt.subplot()
+handles = []
+_plot_title = ''
 
 def createArrayFromDataframes(dataframeArray, columnName):
     bigTempArray = []
@@ -36,29 +39,6 @@ def consolidateArray(arrays):
     for array in arrays:
         consolidatedArray += array
     return consolidatedArray
-
-
-settingsDict = {'1': 'ExcelFiles/Compression/',
-                '2': 'ExcelFiles/Tension/',
-                '3': '3DP/',
-                '4': 'BMF/',
-                '5': 'Median',
-                '6': 'Combined'}
-settings = [input("Compression(1) or Tension(2): "), input("3DP(3) or BMF(4): "),
-            settingsDict[input("Median(5) or Combined(6): ")]]
-filepath = settingsDict[settings[0]] + settingsDict[settings[1]]
-print("Please wait while the graph generates...")
-if settings[1] == '4':
-    key = ['One', 'Two', 'Three']
-else:
-    key = ['Z', 'Y', 'X']
-
-zCubeSheets = createArrayFromFiles(filepath, key[0])
-print("zCubeSheets generated...")
-yCubeSheets = createArrayFromFiles(filepath, key[1])
-print("yCubeSheets generated...")
-xCubeSheets = createArrayFromFiles(filepath, key[2])
-print("xCubeSheets generated...")
 
 
 def findUltimateStrength(dataframeArray, columnName):
@@ -95,17 +75,6 @@ def findMedianTotalArea(dataframeArray):
         # print("areas: " + str(areas))
         areasList.append(sum(areas))
     return median(areasList)
-
-
-# print("median total energy \n proximal-distal: " + str(findMedianTotalArea(zCubeSheets)))
-# print(" cranial-caudal: " + str(findMedianTotalArea(yCubeSheets)))
-# print(" medial-lateral: " + str(findMedianTotalArea(xCubeSheets)))
-#
-# print("Ultimate Z Strength (MPa): " + str(findUltimateStrength(zCubeSheets, 'Stress')))
-# print("Ultimate Y Strength (MPa): " + str(findUltimateStrength(yCubeSheets, 'Stress')))
-# print("Ultimate X Strength (MPa): " + str(findUltimateStrength(xCubeSheets, 'Stress')))
-# input("Stop here")
-# print("Continuing...")
 
 
 def findSmallestArray(array):
@@ -145,145 +114,216 @@ def findMedianMaxStrain(dataframeArray, columnName):
     return closest_array
 
 
-ZFillColor = ''
-YFillColor = ''
-XFillColor = ''
-ZDotColor = ''
-YDotColor = ''
-XDotColor = ''
-if settings[0] == '1':
-    ZFillColor = 'royalblue'
-    YFillColor = 'firebrick'
-    XFillColor = 'limegreen'
-    ZDotColor = 'mediumblue'
-    YDotColor = 'darkred'
-    XDotColor = 'darkgreen'
-elif settings[0] == '2':
-    ZFillColor = 'powderblue'
-    YFillColor = 'lightcoral'
-    XFillColor = 'lightgreen'
-    ZDotColor = 'deepskyblue'
-    YDotColor = 'red'
-    XDotColor = 'lawngreen'
+def hatchBackground(_settings, x, y):
+    high = [n + .15 for n in y]
+    low = [n - .15 for n in y]
+    if _settings[1] == '3':
+        plt.fill_between(x, high, low, hatch='|', alpha=0)
 
 
-if settings[2] == 'Combined':
-    strainZ = createArrayFromDataframes(zCubeSheets, 'Strain')
-    stressZ = createArrayFromDataframes(zCubeSheets, 'Stress')
-    print("zArrays generated")
+def generateGraph():
+    settingsDict = {'1': 'ExcelFiles/Compression/',
+                    '2': 'ExcelFiles/Tension/',
+                    '3': '3DP/',
+                    '4': 'BMF/',
+                    '5': 'Median',
+                    '6': 'Combined'}
+    settings = [input("Compression(1) or Tension(2): "), input("3DP(3) or BMF(4): "),
+                settingsDict[input("Median(5) or Combined(6): ")],
+                input("Which orientations? All that apply (pd, cc, ml): ")]
+    filepath = settingsDict[settings[0]] + settingsDict[settings[1]]
+    print("Please wait while the graph generates...")
+    if settings[1] == '4':
+        key = ['One', 'Two', 'Three']
+    else:
+        key = ['Z', 'Y', 'X']
 
-    strainY = createArrayFromDataframes(yCubeSheets, 'Strain')
-    stressY = createArrayFromDataframes(yCubeSheets, 'Stress')
-    print("yArrays generated")
+    zCubeSheets = None
+    yCubeSheets = None
+    xCubeSheets = None
+    if 'pd' in settings[3]:
+        zCubeSheets = createArrayFromFiles(filepath, key[0])
+        print("zCubeSheets generated...")
+    if 'cc' in settings[3]:
+        yCubeSheets = createArrayFromFiles(filepath, key[1])
+        print("yCubeSheets generated...")
+    if 'ml' in settings[3]:
+        xCubeSheets = createArrayFromFiles(filepath, key[2])
+        print("xCubeSheets generated...")
 
-    strainX = createArrayFromDataframes(xCubeSheets, 'Strain')
-    stressX = createArrayFromDataframes(xCubeSheets, 'Stress')
-    print("xArrays generated")
+    # print("median total energy \n proximal-distal: " + str(findMedianTotalArea(zCubeSheets)))
+    # print(" cranial-caudal: " + str(findMedianTotalArea(yCubeSheets)))
+    # print(" medial-lateral: " + str(findMedianTotalArea(xCubeSheets)))
+    #
+    # print("Ultimate Z Strength (MPa): " + str(findUltimateStrength(zCubeSheets, 'Stress')))
+    # print("Ultimate Y Strength (MPa): " + str(findUltimateStrength(yCubeSheets, 'Stress')))
+    # print("Ultimate X Strength (MPa): " + str(findUltimateStrength(xCubeSheets, 'Stress')))
+    # input("Stop here")
+    # print("Continuing...")
 
-    consolidatedStrainZ = consolidateArray(strainZ)
-    consolidatedStressZ = consolidateArray(stressZ)
+    ZFillColor = ''
+    YFillColor = ''
+    XFillColor = ''
+    ZDotColor = ''
+    YDotColor = ''
+    XDotColor = ''
+    if settings[0] == '1':
+        ZFillColor = 'royalblue'
+        YFillColor = 'firebrick'
+        XFillColor = 'limegreen'
+        ZDotColor = 'mediumblue'
+        YDotColor = 'darkred'
+        XDotColor = 'darkgreen'
+    elif settings[0] == '2':
+        ZFillColor = 'powderblue'
+        YFillColor = 'lightcoral'
+        XFillColor = 'lightgreen'
+        ZDotColor = 'deepskyblue'
+        YDotColor = 'red'
+        XDotColor = 'lawngreen'
 
-    consolidatedStrainY = consolidateArray(strainY)
-    consolidatedStressY = consolidateArray(stressY)
+    if settings[2] == 'Combined':
+        if zCubeSheets:
+            strainZ = createArrayFromDataframes(zCubeSheets, 'Strain')
+            stressZ = createArrayFromDataframes(zCubeSheets, 'Stress')
+            print("zArrays generated")
+            consolidatedStrainZ = consolidateArray(strainZ)
+            consolidatedStressZ = consolidateArray(stressZ)
+            # noinspection PyTypeChecker
+            plt.scatter(consolidatedStrainZ, consolidatedStressZ, s=2, c=ZDotColor)
 
-    consolidatedStrainX = consolidateArray(strainX)
-    consolidatedStressX = consolidateArray(stressX)
-    print("Consolidated Arrays")
+        if yCubeSheets:
+            strainY = createArrayFromDataframes(yCubeSheets, 'Strain')
+            stressY = createArrayFromDataframes(yCubeSheets, 'Stress')
+            print("yArrays generated")
+            consolidatedStrainY = consolidateArray(strainY)
+            consolidatedStressY = consolidateArray(stressY)
+            # noinspection PyTypeChecker
+            plt.scatter(consolidatedStrainY, consolidatedStressY, s=2, c=YDotColor)
 
-    # noinspection PyTypeChecker
-    plt.scatter(consolidatedStrainZ, consolidatedStressZ, s=2, c=ZDotColor)
-    # noinspection PyTypeChecker
-    plt.scatter(consolidatedStrainY, consolidatedStressY, s=2, c=YDotColor)
-    # noinspection PyTypeChecker
-    plt.scatter(consolidatedStrainX, consolidatedStressX, s=2, c=XDotColor)
-    print("Scattered Arrays")
-elif settings[2] == 'Median':
-    medianZStrainArray = findMedianMaxStrain(zCubeSheets, 'Strain')
-    medianZCubesStress = createMedianArray(zCubeSheets, 'Stress', medianZStrainArray, 1)
-    medianZCubesStrain = createMedianArray(zCubeSheets, 'Strain', medianZStrainArray, 1000000)
-    print("zArrays generated")
+        if xCubeSheets:
+            strainX = createArrayFromDataframes(xCubeSheets, 'Strain')
+            stressX = createArrayFromDataframes(xCubeSheets, 'Stress')
+            print("xArrays generated")
+            consolidatedStrainX = consolidateArray(strainX)
+            consolidatedStressX = consolidateArray(stressX)
+            # noinspection PyTypeChecker
+            plt.scatter(consolidatedStrainX, consolidatedStressX, s=2, c=XDotColor)
+    elif settings[2] == 'Median':
+        medianSettingsDict = {
+            'ExcelFiles/Compression/': '_Cube_',
+            'ExcelFiles/Tension/': '_Beam_'
+        }
 
-    medianYStrainArray = findMedianMaxStrain(yCubeSheets, 'Strain')
-    medianYCubesStress = createMedianArray(yCubeSheets, 'Stress', medianYStrainArray, 1)
-    medianYCubesStrain = createMedianArray(yCubeSheets, 'Strain', medianYStrainArray, 1000000)
-    print("yArrays generated")
+        savepath = ('MedianExcelFiles/' + settingsDict[settings[1]][:-1]
+                    + medianSettingsDict[settingsDict[settings[0]]])
 
-    medianXStrainArray = findMedianMaxStrain(xCubeSheets, 'Strain')
-    medianXCubesStress = createMedianArray(xCubeSheets, 'Stress', medianXStrainArray, 1)
-    medianXCubesStrain = createMedianArray(xCubeSheets, 'Strain', medianXStrainArray, 1000000)
-    print("xArrays generated")
+        if zCubeSheets:
+            medianZStrainArray = findMedianMaxStrain(zCubeSheets, 'Strain')
+            medianZCubesStress = createMedianArray(zCubeSheets, 'Stress', medianZStrainArray, 1)
+            medianZCubesStrain = createMedianArray(zCubeSheets, 'Strain', medianZStrainArray, 1000000)
+            print("zArrays generated")
+            z_ci = 2.576 * std(medianZCubesStress) / math.sqrt(len(medianZCubesStress))
+            plt.fill_between(medianZCubesStrain, (medianZCubesStress - z_ci), (medianZCubesStress + z_ci),
+                             color=ZFillColor,
+                             alpha=0.4)
+            # noinspection PyTypeChecker
+            plt.scatter(medianZCubesStrain, medianZCubesStress, s=.3, c=ZDotColor)
+            hatchBackground(settings, medianZCubesStrain, medianZCubesStress)
 
-    medianSettingsDict = {
-        'ExcelFiles/Compression/': '_Cube_',
-        'ExcelFiles/Tension/': '_Beam_'
-    }
+            zCubeMedian_dict = {'Strain': medianZCubesStrain, 'Median Stress': medianZCubesStress}
+            zCubeDataframe = pd.DataFrame(data=zCubeMedian_dict)
+            zCubeDataframe.to_excel(savepath + key[0] + '_Median.xlsx')
 
-    savepath = ('MedianExcelFiles/' + settingsDict[settings[1]][:-1]
-                + medianSettingsDict[settingsDict[settings[0]]])
+        if yCubeSheets:
+            medianYStrainArray = findMedianMaxStrain(yCubeSheets, 'Strain')
+            medianYCubesStress = createMedianArray(yCubeSheets, 'Stress', medianYStrainArray, 1)
+            medianYCubesStrain = createMedianArray(yCubeSheets, 'Strain', medianYStrainArray, 1000000)
+            print("yArrays generated")
+            y_ci = 2.576 * std(medianYCubesStress) / math.sqrt(len(medianYCubesStress))
+            plt.fill_between(medianYCubesStrain, (medianYCubesStress - y_ci), (medianYCubesStress + y_ci),
+                             color=YFillColor,
+                             alpha=0.4)
+            # noinspection PyTypeChecker
+            plt.scatter(medianYCubesStrain, medianYCubesStress, s=.3, c=YDotColor)
+            hatchBackground(settings, medianYCubesStrain, medianYCubesStress)
 
-    z_ci = 2.576 * std(medianZCubesStress) / math.sqrt(len(medianZCubesStress))
-    y_ci = 2.576 * std(medianYCubesStress) / math.sqrt(len(medianYCubesStress))
-    x_ci = 2.576 * std(medianXCubesStress) / math.sqrt(len(medianXCubesStress))
+            yCubeMedian_dict = {'Strain': medianYCubesStrain, 'Median Stress': medianYCubesStress}
+            yCubeDataframe = pd.DataFrame(data=yCubeMedian_dict)
+            yCubeDataframe.to_excel(savepath + key[1] + '_Median.xlsx')
 
-    plt.fill_between(medianZCubesStrain, (medianZCubesStress - z_ci), (medianZCubesStress + z_ci),
-                     color=ZFillColor,
-                     alpha=0.4)
-    # noinspection PyTypeChecker
-    plt.scatter(medianZCubesStrain, medianZCubesStress, s=.3, c=ZDotColor)
+        if xCubeSheets:
+            medianXStrainArray = findMedianMaxStrain(xCubeSheets, 'Strain')
+            medianXCubesStress = createMedianArray(xCubeSheets, 'Stress', medianXStrainArray, 1)
+            medianXCubesStrain = createMedianArray(xCubeSheets, 'Strain', medianXStrainArray, 1000000)
+            print("xArrays generated")
+            x_ci = 2.576 * std(medianXCubesStress) / math.sqrt(len(medianXCubesStress))
+            plt.fill_between(medianXCubesStrain, (medianXCubesStress - x_ci), (medianXCubesStress + x_ci),
+                             color=XFillColor,
+                             alpha=0.4)
+            # noinspection PyTypeChecker
+            plt.scatter(medianXCubesStrain, medianXCubesStress, s=.3, c=XDotColor)
+            hatchBackground(settings, medianXCubesStrain, medianXCubesStress)
 
-    plt.fill_between(medianYCubesStrain, (medianYCubesStress - y_ci), (medianYCubesStress + y_ci),
-                     color=YFillColor,
-                     alpha=0.4)
-    # noinspection PyTypeChecker
-    plt.scatter(medianYCubesStrain, medianYCubesStress, s=.3, c=YDotColor)
+            xCubeMedian_dict = {'Strain': medianXCubesStrain, 'Median Stress': medianXCubesStress}
+            xCubeDataframe = pd.DataFrame(data=xCubeMedian_dict)
+            xCubeDataframe.to_excel(savepath + key[2] + '_Median.xlsx')
 
-    plt.fill_between(medianXCubesStrain, (medianXCubesStress - x_ci), (medianXCubesStress + x_ci),
-                     color=XFillColor,
-                     alpha=0.4)
-    # noinspection PyTypeChecker
-    plt.scatter(medianXCubesStrain, medianXCubesStress, s=.3, c=XDotColor)
+    if settings[1] == '3':
+        print("hatching label")
+        hatch = '|||'
+    else:
+        print("not hatching label")
+        hatch = ''
+    if zCubeSheets:
+        ZCube = mpatches.Patch(color=ZDotColor, hatch=hatch, label=settingsDict[settings[1]][:-1] + ' Proximal-Distal ' + settingsDict[settings[0]][11:-1])
+        handles.append(ZCube)
+    if yCubeSheets:
+        YCube = mpatches.Patch(color=YDotColor, hatch=hatch, label=settingsDict[settings[1]][:-1] + ' Cranial-Caudal ' + settingsDict[settings[0]][11:-1])
+        handles.append(YCube)
+    if xCubeSheets:
+        XCube = mpatches.Patch(color=XDotColor, hatch=hatch, label=settingsDict[settings[1]][:-1] + ' Medial-Lateral ' + settingsDict[settings[0]][11:-1])
+        handles.append(XCube)
+    plotTitle = 'Stress vs Strain on '
+    if settings[2].lower() == 'median':
+        plotTitle += 'median of '
+    if 'pd' in settings[3] and 'cc' in settings[3] and 'ml' in settings[3]:
+        plotTitle += 'each orientation of '
+    if settings[1] == '3':
+        plotTitle += '\n 9mm\u00B3 Formlabs '
+    elif settings[1] == '4':
+        plotTitle += '\n 3mm\u00B3 BMF '
+    if settings[0] == '1':
+        plotTitle += 'Cube in Compression'
+    elif settings[0] == '2':
+        plotTitle += 'Beam in Tension'
+    _plot_title = plotTitle
 
-    zCubeMedian_dict = {'Strain': medianZCubesStrain, 'Median Stress': medianZCubesStress}
-    zCubeDataframe = pd.DataFrame(data=zCubeMedian_dict)
-    zCubeDataframe.to_excel(savepath + key[0] + '_Median.xlsx')
 
-    yCubeMedian_dict = {'Strain': medianYCubesStrain, 'Median Stress': medianYCubesStress}
-    yCubeDataframe = pd.DataFrame(data=yCubeMedian_dict)
-    yCubeDataframe.to_excel(savepath + key[1] + '_Median.xlsx')
+generating = True
+graphCount = 0
+while generating:
+    generating = False
+    generateGraph()
+    graphCount += 1
+    request = str(input("Would you like to generate another graph? (y/n): "))
+    print(request)
+    if 'y' in request.lower():
+        generating = True
 
-    xCubeMedian_dict = {'Strain': medianXCubesStrain, 'Median Stress': medianXCubesStress}
-    xCubeDataframe = pd.DataFrame(data=xCubeMedian_dict)
-    xCubeDataframe.to_excel(savepath + key[2] + '_Median.xlsx')
-
-ax = plt.subplot()
-
-ZCube = mpatches.Patch(color='blue', label='Proximal-Distal')
-YCube = mpatches.Patch(color='red', label='Cranial-Caudal')
-xCube = mpatches.Patch(color='green', label='Medial-Lateral')
-ax.legend(handles=[xCube, YCube, ZCube])
+ax.legend(handles=handles)
 
 plt.xlabel('Microstrain (\u03BC\u03B5)')
 plt.ylabel('Stress (MPa)')
 
-plotTitle = 'Stress vs Strain on '
-if settings[2].lower() == 'median':
-    plotTitle += 'median of '
-if input("all orientations? (y/n): ").lower == 'y':
-    plotTitle += 'each orientation of '
-if settings[1] == '3':
-    plotTitle += '\n 9mm\u00B3 Formlabs '
-elif settings[1] == '4':
-    plotTitle += '\n 3mm\u00B3 BMF '
-if settings[0] == '1':
-    plotTitle += 'Cube in Compression'
-elif settings[0] == '2':
-    plotTitle += 'Beam in Tension'
-
-plt.title('Stress vs Strain on median of each orientation of \n 9mm\u00B3 Formlabs Cube in Compression')
+if graphCount > 1:
+    plt.title = input("What graphs are you comparing (A vs B)?: ")
+else:
+    plt.title = _plot_title
 plt.xlim(0, 400000)
 plt.ylim(0, 17.5)
-plt.show()
 savename = input("Save file with name: ")
-#plt.savefig('../Final Graphs/" + savename + ".png', transparent=True)
+plt.savefig('../Final Graphs/' + savename + '.png', transparent=True)
+plt.show()
 
 input()
